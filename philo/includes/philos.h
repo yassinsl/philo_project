92c2fa00ec7202aaf327131a31158ce243103b1e
yassinsl <slawiyassin152@gmail.com>
2025-05-16 15:24:48 +0100

    Update :)

--- philo/includes/philos.h
+++ philo/includes/philos.h
@@ -1,8 +1,13 @@
 #ifndef PHILOS_H
 #define PHILOS_H
+
 #include <limits.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <stdbool.h>
 #include "../Libft/libft.h"
 
 #define TRUE 1
@@ -10,27 +15,50 @@
 #define INVALID_NUMBER "Error: Invalid number of arguments\n\
         Usage: ./philo number_of_philosophers time_to_die time_to_eat time_to_sleep\
         [number_of_times_each_philosopher_must_eat]\n"
-#define INVALID_ARGS "Error: Invalid Argements\n"
-//this the struct to get a valid argement;
+#define INVALID_ARGS "Error: Invalid Arguments\n"
 
-typedef s_create_philos
+typedef struct s_data_philos t_data_philosophers;
+
+typedef struct s_create_philos
 {
-	int id;
-	int left_fork;
-	int right_fork;
-	int last_meal_time;
-	int meals_eaten;
-	int is_dead;
-	t_data_philosophers *data;
-} s_create_philos;
+    int id;
+    int left_fork;
+    int right_fork;
+    long long last_meal_time;
+    int meals_eaten;
+    int is_dead;
+    pthread_t thread;
+    t_data_philosophers *data;
+} t_philos;
+
 typedef struct s_data_philos
 {
     int number_of_philosophers;
     int time_to_die;
     int time_to_eat;
     int time_to_sleep;
-    char *forks;
-    t_create_philosophers *philos;
+    int number_of_times_each_philosopher_must_eat;
+    int idx;
+    int someone_died;
+    long long start_time;
+    pthread_mutex_t *forks;
+    pthread_mutex_t print_mutex;
+    pthread_mutex_t death_mutex;
+    t_philos *philos;
 } t_data_philosophers;
+
+// Function prototypes
+void ft_error_msg(char *msg);
+int ft_atoi(char *str);
+long long get_current_time(void);
+long long get_timestamp(t_data_philosophers *data);
+void create_mutexes(t_data_philosophers *philos);
+void create_philosophers(t_data_philosophers *data);
+void get_data_from_args(t_data_philosophers *philos_data, char **av);
+void think_random_time(int id);
+void *create_a_philos_routine(void *arg);
+void *monitor_routine(void *arg);
+void free_resources(t_data_philosophers *philos);
+void create_threads(t_data_philosophers *philos);
 
 #endif
--- philo/main.c
+++ philo/main.c
@@ -31,45 +31,10 @@ int ft_atoi(char *str)
         ft_error_msg(INVALID_ARGS);
     return((int)(sign * num));
 }
-void create_mutexes(t_data_philosophers *philos) {
-    int i;
-    
-    i = -1;
-    philos->forks = malloc(sizeof(pthread_mutex_t) * philos->number_of_philosophers);
-    if (!philos->forks)
-        ft_error_msg(ERROR_ALC);
-    while (++i < philos->number_of_philosophers)
-        if (pthread_mutex_init(&philos->forks[i], NULL))
-            ft_error_msg(MUTEX_FAILED);
-    if (pthread_mutex_init(&philos->print_mutex, NULL)||
-        pthread_mutex_init(&philos->death_mutex, NULL))
-        ft_error_msg(MUTEX_FAILED);
-}
-
-void create_philosophers(t_data_philosophers *data)
-{
-    int i;
-    
-    data->philos = malloc(sizeof(t_philos) * data->number_of_philosophers);
-    if (!data->philos)
-        ft_error_msg(ERROR_ALC);
-    
-    i = -1;
-    while (++i < data->number_of_philosophers)
-    {
-        data->philos[i].id = i + 1;
-        data->philos[i].left_fork = i;
-        data->philos[i].right_fork = (i + 1) % data->number_of_philosophers;
-        data->philos[i].last_meal_time = get_current_time();
-        data->philos[i].meals_eaten = 0;
-        data->philos[i].is_dead = 0;
-        data->philos[i].data = data;
-    }
-}
 
 void get_data_from_args(t_data_philosophers *philos_data, char **av) 
 {
-    philos_data->number_of_philosophers = ft_atoi(av[1;]);
+    philos_data->number_of_philosophers = ft_atoi(av[1]);
     philos_data->time_to_die = ft_atoi(av[2]);
     philos_data->time_to_eat = ft_atoi(av[3]);
     philos_data->time_to_sleep = ft_atoi(av[4]);
@@ -79,49 +44,56 @@ void get_data_from_args(t_data_philosophers *philos_data, char **av)
     else
         philos_data->number_of_times_each_philosopher_must_eat = -1;
 }
-void think_random_time(int id)
+//    ms = ((time.tv_usec + id * 1000) % 400 + 100) * 1000; //range the time bet 100 and 499 ms  
+
+//here i creat a mutex tomorrow read about the mutex how to work in the kernel;
+void create_a_mutex(t_data_philosophers *philos)
+{
+    philos->fork = malloc(sizeof(pthread_metux_t) * philos->number_of_philosophers);
+    if(!fork)
+        ft_error_msg(MUTEX_FAILED);
+    int i = -1;
+    while(i++ < philos->number_of_philosophers)
+        pthread_mutex_init(&fork[i], NULL);
+    pthread_mutex_init(philos->print_mutex, NULL);
+    pthread_mutex_init(philos->death_mutex, NULL);
+}
+void  get_current_time(void)
 {
     t_time time;
-    int ms
-    
-    gettimofday(&time, NULL);
-    ms = ((time.tv_usec + id * 1000) % 400 + 100) * 1000; //range the time bet 100 and 499 ms  
-    usleep(ms);
+    gettimeofday(time, NULL);
+    return(time.tv_sec * 1000  + time.tv_usec  / 1000);
 }
-void create_a_philos_routine(t_data_philosophers *philos)
-{   
-    t_create_philosophers *philos_data =  data->philos;
+//here i creat a data philos
+void create_philosophers(t_data_philosophers *data)
+{
+    int i = 0;
 
-    while(1)
+    data->philos = malloc(sizeof(t_philos) * philos.number_of_philosophers);
+    if(!data->philos)
+        ft_error_msg(ERROR_ALC);
+    while(i++ < philos_data.number_of_philosophers)
     {
-        printf("Philosopher %d is thinking\n", id);
-        think_random_time(philos_data->id);
-        while (1) { 
-            if (pthread_mutex_trylock(&forks[philos_data->left_fork]) == 0) {
-                if (pthread_mutex_trylock(&forks[right_fork]) == 0) 
-                {
-                    printf("Philosopher %d is eating\n", id);
-                    random_sleep(id);
-                    pthread_mutex_unlock(&forks[right]);
-                }
-                pthread_mutex_unlock(&forks[left]);
-            } 
-            else 
-                usleep(1000); 
-        }
-        
+        data->philos[i].id = i + 1;
+        data->philos[i].left_fork = i;
+        data->philos[i].right_fork = (i + 1) % data->number_of_philosophers;
+        data->philos[i].last_meal_eaten = get_current_time(); 
+        data->philos[i].meals_eaten = 0;
+        data->philos[i].is_dead = 0;
+        data->philos[i].data = data;  
     }
 }
+//void create_a_philos_routine()
 int main(int ac, char *av[])
 {
     t_data_philosophers philos_data;
 
     philos_data = (t_data_philosophers){0};
-    //here i check the if the args are validet :)
-    if(ac  != 5 || ac != 6)
+    if(ac  < 5 && ac > 6)
         ft_error_msg(INVALID_NUMBER);
-    get_data_from_args(&philos_data, av); 
+    get_data_from_args(&philos_data, av);
+    create_a_mutex(&philos_data) 
     create_philosophers(&philos_data);
-    create_a_philos_routine(&philos_data);
+    //create_a_philos_routine(&philos_data);
     return FALSE;
 }
