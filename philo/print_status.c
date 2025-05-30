988d72ee00fcbe0f3c9ab99e542414a12680872f
yassinsl <slawiyassin152@gmail.com>
2025-05-30 15:09:03 +0100

    d

--- philo/print_status.c
+++ philo/print_status.c
@@ -6,7 +6,7 @@
 /*   By: ylahssin <ylahssin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 14:56:23 by ylahssin          #+#    #+#             */
-/*   Updated: 2025/05/30 14:31:01 by ylahssin         ###   ########.fr       */
+/*   Updated: 2025/05/30 15:00:04 by ylahssin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,10 @@
  * print_status - Print status for each philos
  * Return: NON Return
  */
-void print_status(t_philos *philo, char *msg)
+void	print_status(t_philos *philo, char *msg)
 {
-    pthread_mutex_lock(philo->data->death_mutex);
-    if (philo->data->someone_died) 
-    {
-        pthread_mutex_unlock(philo->data->death_mutex);
-        return;
-    }
-    pthread_mutex_unlock(philo->data->death_mutex);
-    
-    pthread_mutex_lock(philo->data->print_mutex);
-    printf("%lld %d %s\n", get_timestamp(philo->data), philo->id, msg);
-    pthread_mutex_unlock(philo->data->print_mutex);
+	pthread_mutex_lock(philo->data->print_mutex);
+	if (!philo->data->someone_died)
+		printf("%lld %d %s\n", get_timestamp(philo->data), philo->id, msg);
+	pthread_mutex_unlock(philo->data->print_mutex);
 }
